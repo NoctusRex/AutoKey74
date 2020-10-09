86195fe8199b5d814aec44c320548a3d54f53bc3
Noctus Rex <noxjudex@gmail.com>
2020-10-09 16:58:32 +0200

    Added exception handling and running detection

--- AutoKey74/App.xaml.cs
+++ AutoKey74/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace AutoKey74
 {
@@ -13,5 +15,40 @@ namespace AutoKey74
     /// </summary>
     public partial class App : Application
     {
+        private Mutex mutex;
+        private bool handleExceptions = false;
+
+        private void Application_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = handleExceptions;
+
+            MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            CheckAlreadyRunning();
+
+            base.OnStartup(e);
+        }
+
+        private void CheckAlreadyRunning()
+        {
+            mutex = new Mutex(false, "AutoKey74 - 133742069");
+
+            if (!mutex.WaitOne(0, false))
+                throw new Exception("The application is already running.");
+
+            handleExceptions = true;
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            mutex?.Close();
+            mutex?.Dispose();
+            mutex = null;
+
+            base.OnExit(e);
+        }
     }
 }
